1db21d7ad0f6135f325619c1f39d00d7338777c1
anilkuj <anilkuj@gmail.com>
2012-03-29 01:29:20 -0400

    Rlslog initial release

--- plugin.video.rlslog/default.py
+++ plugin.video.rlslog/default.py
@@ -5,7 +5,6 @@ import urlresolver
 from t0mm0.common.addon import Addon
 from t0mm0.common.net import Net
 import HTMLParser
-from BeautifulSoup import BeautifulStoneSoup
 
 try:
 	from sqlite3 import dbapi2 as sqlite
@@ -14,21 +13,33 @@ except:
 	from pysqlite2 import dbapi2 as sqlite
 	print "Loading pysqlite2 as DB engine"
 
+addon_id = 'plugin.video.rlslog'
+plugin = xbmcaddon.Addon(id=addon_id)
 
 DB = os.path.join(xbmc.translatePath("special://database"), 'rlslog.db')
-BASE_URL = 'http://www.rlslog.eu'
+BASE_URL = 'http://www.rlslog.net'
 net = Net()
 addon = Addon('plugin.video.rlslog', sys.argv)
+showAllParts = True
+showPlayAll = True
+
+if plugin.getSetting('showAllParts') == 'false':
+        showAllParts = False
+
+if plugin.getSetting('showPlayAll') == 'false':
+        showPlayAll = False
 
 ##### Queries ##########
 mode = addon.queries['mode']
 url = addon.queries.get('url', None)
-section = addon.queries.get('section', None)
+content = addon.queries.get('content', None)
 query = addon.queries.get('query', None)
 startPage = addon.queries.get('startPage', None)
 numOfPages = addon.queries.get('numOfPages', None)
-content = addon.queries.get('content', None)
 listitem = addon.queries.get('listitem', None)
+urlList = addon.queries.get('urlList', None)
+section = addon.queries.get('section', None)
+
 
 
 def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
@@ -36,46 +47,59 @@ def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
 
         # handle paging
         pageUrl = url
-        if int(startPage) > 1:
-                pageUrl = url + '/page/' + startPage
+        if int(startPage)> 1:
+                pageUrl = url + 'page/' + startPage + '/'
         print pageUrl
         html = net.http_GET(pageUrl).content
+
         start = int(startPage)
         end = start + int(numOfPages)
-        last = 2
-        match  = re.search("/([\d]+)' class='last'", html)
-        if match:
-                last = int(match.group(1))
 
-        print str(start) + ' : ' + str(end) + ' : ' + str(last)
-        for page in range( start, min(last, end)):
+        for page in range( start, end):
                 if ( page != start):
-                        pageUrl = url + '/page/' + str(page)
+                        pageUrl = url + 'page/' + str(page) + '/'
                         html = net.http_GET(pageUrl).content
-                ListTitles(section, html)
-       	if end < last:
-                addon.add_directory({'mode': 'GetTitles', 'section': section, 'url': url, 'startPage': str(end), 'numOfPages': numOfPages}, {'title':  'Next..'})
+                        
+                match = re.compile('entrytitle.+?href="(.+?)".+?>(.+?)<.+?src="(.+?)"', re.DOTALL).findall(html)
+                for movieUrl, name, img in match:
+                        cm  = []
+                        runstring = 'XBMC.Container.Update(plugin://plugin.video.rlslog/?mode=Search&query=%s)' %(name.strip())
+        		cm.append(('Search on rlslog', runstring))
+                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': movieUrl}, {'title':  name.strip()}, contextmenu_items= cm, img= img)
+                if 'Next Page' not in html:
+                        break
+
+        # keep iterating until the laast page is reached
+        if 'Next Page' in html:
+                addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': str(end), 'numOfPages': numOfPages}, {'title': 'Next...'})
+        
        	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def ListTitles(section, html):
-        print ' in ListTitles'
-        html = html.encode('utf-8')
-        #match = re.compile('entry clearfix.+?href="(.+?)">(.+?)<.+?href="(.+?)"(.+?)<div.+?<p.*?>(.+?)<.+?/title/(.+?)/.+?Download.+?</div>(.+?)</div>', re.DOTALL).findall(html)
-        match = re.compile('entry clearfix.+?href="(.+?)">(.+?)<.+?href="(.+?)".+?Download.+?</div>(.+?)</div>', re.DOTALL).findall(html)
-        #for url, title, img, info, plot, imdbid, content in match:
-        for url, title, img, content in match:
-                addon.add_directory({'mode': 'GetLinks', 'url': url, 'content': content}, {'title':  title}, img= img)
+def GetLinks(section, url): # Get Links
+        print '***************************************************** In GetLinks %s' % url
+        html = net.http_GET(url).content
+        listitem = GetMediaInfo(html)
+        content = html
+        r = re.search('<strong>Links.*</strong>', html)
+        if r:
+                content = html[r.end():]
 
-
-def GetLinks(origUrl, content): # Get TV Links
-        print 'In GetLinks'
-        #html = net.http_GET(url).content
+        r = re.search('commentblock', content)
+        if r:
+                content = content[:r.start()]
+                
         match = re.compile('href="(.+?)"').findall(content)
         listitem = GetMediaInfo(content)
         for url in match:
                 host = GetDomain(url)
+
                 if 'Unknown' in host:
+                                continue
+                        
+                # ignore .rar files
+                r = re.search('\.rar[(?:\.html|\.htm)]*', url, re.IGNORECASE)
+                if r:
                         continue
                 print '*****************************' + host + ' : ' + url
                 if urlresolver.HostedMediaFile(url= url):
@@ -84,27 +108,32 @@ def GetLinks(origUrl, content): # Get TV Links
                         title = title[2].replace('.html', '')
                         title = title.replace('.htm', '')
                         addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  host + ' : ' + title})
-        print ' ------------------------ %s'% origUrl
-        html = net.http_GET(origUrl).content
-        find = re.search('<div id="comment-wrap">', html)
+
+        find = re.search('commentblock', html)
         if find:
                 print 'in comments if'
                 html = html[find.end():]
-                match = re.compile('<a href="(.+?)"', re.DOTALL).findall(html)
+                match = re.compile('<a href="(.+?)" rel="nofollow"', re.DOTALL).findall(html)
                 print len(match)
                 for url in match:
                         host = GetDomain(url)
                         if 'Unknown' in host:
                                 continue
-                        print '*****************************' + host + ' : ' + url
-                        if urlresolver.HostedMediaFile(url= url):
-                                print 'in GetLinks if loop'
-                                title = url.rpartition('/')
-                                title = title[2].replace('.html', '')
-                                title = title.replace('.htm', '')
-                                addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  host + ' : ' + title})
+                        
+                        # ignore .rar files
+                        r = re.search('\.rar[(?:\.html|\.htm)]*', url, re.IGNORECASE)
+                        if r:
+                                continue
+                        try:
+                                if urlresolver.HostedMediaFile(url= url):
+                                        print 'in GetLinks if loop'
+                                        title = url.rpartition('/')
+                                        title = title[2].replace('.html', '')
+                                        title = title.replace('.htm', '')
+                                        addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  host + ' : ' + title})
+                        except:
+                                continue
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
 
 def PlayVideo(url, listitem):
         print 'in PlayVideo %s' % url
@@ -123,35 +152,30 @@ def GetMediaInfo(html):
         listitem = xbmcgui.ListItem()
         match = re.search('og:title" content="(.+?) \((.+?)\)', html)
         if match:
-                print match.group(1) + ' : ' + match.group(2)
+                print match.group(1) + ' : '  + match.group(2)
                 listitem.setInfo('video', {'Title': match.group(1), 'Year': int(match.group(2)) } )
         return listitem
 
 def Categories(section):  #categories
 
+        url = BASE_URL + '/category/' + section
         html = net.http_GET(BASE_URL).content
-        addon.add_directory({'mode': 'GetTitles', 'section': section, 'url': BASE_URL + '/category/' + section,
-                             'startPage': '1', 'numOfPages': '2'}, {'title':  'All'})
-
-        match = re.compile('class="cat-item.+?/category/' + section + '/(.+?)".+?>(.+?)<').findall(html)
+        match = re.compile('<li class=.+?/category/' + section + '(.+?)".+?>(.+?)<').findall(html)
         for cat, title in match:
-                url = BASE_URL + '/category/' + section + '/' + cat
+                url = url + cat
                 addon.add_directory({'mode': 'GetTitles', 'section': section, 'url': url,
                                      'startPage': '1', 'numOfPages': '2'}, {'title':  title})
-        
-        addon.add_directory({'mode': 'GetSearchQuery', 'section': section},  {'title':  'Search'})
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def MainMenu():    #homescreen
         addon.add_directory({'mode': 'Categories', 'section': 'movies'},  {'title':  'Movies'})
         addon.add_directory({'mode': 'Categories', 'section': 'tv-shows'},  {'title':  'TV Shows'})
-        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/staff-recommended',
-                             'startPage': '1', 'numOfPages': '2'}, {'title':  'Staff Recommended'})
+        addon.add_directory({'mode': 'GetSearchQuery'},  {'title':  'Search'})
         addon.add_directory({'mode': 'ResolverSettings'}, {'title':  'Resolver Settings'})
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
-def GetSearchQuery(section):
+def GetSearchQuery():
 	last_search = addon.load_data('search')
 	if not last_search: last_search = ''
 	keyboard = xbmc.Keyboard()
@@ -161,32 +185,36 @@ def GetSearchQuery(section):
 	if (keyboard.isConfirmed()):
                 query = keyboard.getText()
                 addon.save_data('search',query)
-                Search(section, query)
+                Search(query)
 	else:
                 return
 
-def Search(section, query):
-        url = BASE_URL + '/?s='  + query
+        
+def Search(query):
+        url = 'http://www.google.com/search?q=site:rlslog.net ' + query
+        url = url.replace(' ', '+')
         print url
-        GetTitles(section, url, startPage= '1', numOfPages= '1')
+        html = net.http_GET(url).content
+        match = re.compile('<h3 class="r"><a href="(.+?)".+?onmousedown=".+?">(.+?)</a>').findall(html)
+        for url, title in match:
+                title = title.replace('<b>...</b>', '').replace('<em>', '').replace('</em>', '')
+                addon.add_directory({'mode': 'GetLinks', 'url': url}, {'title':  title})
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
 
 if mode == 'main': 
 	MainMenu()
-elif mode == 'BrowseGenre':
-	BrowseGenre(content)
 elif mode == 'GetTitles': 
 	GetTitles(section, url, startPage, numOfPages)
-elif mode == 'ListMovies': 
-	ListMovies(section, url)
 elif mode == 'GetLinks':
-	GetLinks(url, content)
+	GetLinks(section, url)
 elif mode == 'GetSearchQuery':
-	GetSearchQuery(section)
+	GetSearchQuery()
 elif mode == 'Search':
 	Search(query)
-elif mode == 'Categories':
-	Categories(section)
+elif mode == 'PlayVideo':
+	PlayVideo(url, listitem)	
 elif mode == 'ResolverSettings':
         urlresolver.display_settings()
-elif mode == 'PlayVideo':
-	PlayVideo(url, listitem)
+elif mode == 'Categories':
+        Categories(section)
