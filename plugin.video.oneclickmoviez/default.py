c44b854a0657b4920187837c2281e32a32e9ba64
anilkuj <anilkuj@gmail.com>
2012-04-02 12:39:14 -0400

    Updated OneClickMovies with metadata support

--- plugin.video.oneclickmoviez/default.py
+++ plugin.video.oneclickmoviez/default.py
@@ -4,7 +4,8 @@ import re, string, sys, os
 import urlresolver
 from t0mm0.common.addon import Addon
 from t0mm0.common.net import Net
-import HTMLParser
+from metahandler import metahandlers
+
 
 try:
 	from sqlite3 import dbapi2 as sqlite
@@ -12,6 +13,10 @@ try:
 except:
 	from pysqlite2 import dbapi2 as sqlite
 	print "Loading pysqlite2 as DB engine"
+
+
+prepare_zip = False
+metaget=metahandlers.MetaData(preparezip=prepare_zip)
 
 addon_id = 'plugin.video.oneclickmoviez'
 plugin = xbmcaddon.Addon(id=addon_id)
@@ -23,12 +28,16 @@ addon = Addon('plugin.video.oneclickmoviez', sys.argv)
 
 hideAdult = True
 autoPlay = True
+enableMeta = True
 
 if plugin.getSetting('hideAdult') == 'false':
         hideAdult = False
 
 if plugin.getSetting('autoPlay') == 'false':
         autoPlay = False
+
+if plugin.getSetting('enableMeta') == 'false':
+        enableMeta = False
 
 ##### Queries ##########
 mode = addon.queries['mode']
@@ -45,8 +54,8 @@ year = addon.queries.get('year', None)
 
 
 
-def GetTitles(url, startPage= '1', numOfPages= '1'): # Get Movie Titles
-        print 'oneclickmoviez get Movie Titles Menu %s' % url
+def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
+        print 'oneclickmoviez get Movie Titles Menu %s' % section
 
         # handle paging
         pageUrl = url
@@ -56,31 +65,143 @@ def GetTitles(url, startPage= '1', numOfPages= '1'): # Get Movie Titles
 
         start = int(startPage)
         end = start + int(numOfPages)
-
+        print str(start) + ' : ' + str(end)
         for page in range( start, end):
+                print 'in for loop'
                 if ( page != start):
                         pageUrl = url + 'page/' + str(page) + '/'
                         html = net.http_GET(pageUrl).content
-                        
-                match = re.compile('#content.+?href="(.+?)".+?>(.+?)<.+?src="(.+?)".+?/IMDB/(.+?)"', re.DOTALL).findall(html)
-                for movieUrl, name, img, imdbUrl in match:
-                        name = name.strip()
-                        r = re.search('^(.+?) ([\d][\d][\d][\d]) ', name)
-                        year = '0'
-                        title = ''
-                        if r:
-                                title, year = r.groups()
-                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': movieUrl, 'title': title, 'year': year}, {'title':  name}, img= img)
+                match = re.compile('<h2><a href=(.+?)</a></p>', re.DOTALL).findall(html)
+                for content in match:
+                        if section == 'tvshow':
+                                ListTVTitle(section, content)
+                        else:
+                                ListMovieTitle(section, content)
+
                 if 'Previous Entries' not in html:
                         break
-
         # keep iterating until the laast page is reached
         if 'Previous Entries' in html:
-                addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': str(end), 'numOfPages': numOfPages}, {'title': 'Next...'})
+                addon.add_directory({'mode': 'GetTitles', 'section': section, 'url': url, 'startPage': str(end), 'numOfPages': numOfPages}, {'title': 'Next...'})
         
        	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
+def ListTVTitle(section, content):
+        print 'in ListTVTitle'
+        match = re.compile('"(.+?)".+?>(.+?)<.+?src="(.+?)"', re.DOTALL).findall(content)
+        for url, name, img in match:
+                meta = {}
+                r = re.search( '^(.+?) S\d\d', name.strip())
+                title = name
+                if r:
+                        title = r.group(1)
+                if enableMeta:
+                        try:
+                                meta = metaget.get_meta(section, title)
+                        except Exception, e:
+                                print '************* Error querying metadata: %s' % e
+                                meta['title'] = name
+                                meta['backdrop_url'] = ''
+                                meta['cover_url'] = img
+
+                        #if meta['cover_url'] in ('/images/noposter.jpg',''):
+                        meta['cover_url'] = img
+                                
+                        if meta['title']:
+                                name = meta['title']
+                else:
+                        meta['title'] = name
+                        meta['backdrop_url'] = ''
+                        meta['cover_url'] = img
+                        
+                addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': url, 'title': name, 'year': year}, meta,
+                                            img= meta['cover_url'], fanart= meta['backdrop_url'])
+
+def  ListMovieTitle(section, content):
+        print 'in ListMovieTitle'
+        match = re.compile('"(.+?)".+?>(.+?)<.+?src="(.+?)".+?\.com/([\w\./:]+)" target="_blank">IMDB', re.DOTALL).findall(content)
+        for url, name, img, imdbUrl in match:
+                meta = {}
+                r = re.search('^(.+?) [\[]*([\d][\d][\d][\d])[\]]*', name.strip())
+                year = ''
+                title = name
+                if r:
+                        title, year = r.groups()
+                imdbId = GetImdbId(imdbUrl)
+                cm = []
+                if enableMeta:
+                        try:
+                                #print imdbId + ' : ' + title + ' : ' + year
+                                if imdbId:
+                                        meta = metaget.get_meta(section, title, year= year, imdb_id= imdbId )
+                                else:
+                                        meta = metaget.get_meta(section, title, year= year)
+                        except Exception, e:
+                                print '************* Error querying metadata: %s' % e
+                                meta['title'] = name
+                                meta['backdrop_url'] = ''
+                                meta['cover_url'] = img
+
+                        if meta['cover_url'] in ('/images/noposter.jpg',''):
+                                print 'in if'
+                                meta['cover_url'] = img
+
+                        print 'imdb title is %s' % meta['title']
+                        if meta['title']:
+                                name = meta['title']
+
+                        if meta['trailer_url']:
+                                trurl = meta['trailer_url']
+                                trurl = re.sub('&feature=related','',trurl)
+                                trurl = trurl.encode('base-64').strip()
+                                #runstring = 'XBMC.Container.Update(%s?mode=PlayTrailer&url=%s)' %(sys.argv[0], trurl)
+                                runstring = 'XBMC.RunPlugin(%s?mode=PlayTrailer&url=%s)' %(sys.argv[0], trurl)
+                                cm.append(('Watch Trailer', runstring))
+                        cm.append(('Show Information', 'XBMC.Action(Info)'))
+                else:
+                        meta['title'] = name
+                        meta['backdrop_url'] = ''
+                        meta['cover_url'] = img
+                
+                try:        
+                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': url, 'title': name, 'year': year}, meta,
+                                            img= meta['cover_url'], fanart= meta['backdrop_url'], contextmenu_items= cm, context_replace=True)
+                except UnicodeEncodeError, err:
+                        meta['title'] = meta['title'].encode('utf-8')
+                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': url, 'title': title, 'year': year}, meta,
+                                            img= meta['cover_url'], fanart= meta['backdrop_url'], contextmenu_items= cm, context_replace=True)
+                except UnicodeDecodeError, err:
+                        meta['title'] = meta['title'].decode('utf-8')
+                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': url, 'title': title, 'year': year}, meta,
+                                            img= meta['cover_url'], fanart= meta['backdrop_url'], contextmenu_items= cm, context_replace=True)
+                
+
+def PlayTrailer(url):
+	url = url.decode('base-64')
+	print 'Attempting to resolve and play trailer at %s' % url
+	sources = []
+	hosted_media = urlresolver.HostedMediaFile(url=url)
+	sources.append(hosted_media)
+	source = urlresolver.choose_source(sources)
+	if source: stream_url = source.resolve()
+	else: stream_url = ''
+	xbmc.Player().play(stream_url)
+
+def GetImdbId(imdbUrl):
+        print 'imdbUrl is %s' % imdbUrl
+        imdbId = None
+        match = re.search('/title/(.+?)/', imdbUrl)
+        if match:
+                imdbId = match.group(1)
+                return imdbId
+        url = net.http_GET(BASE_URL + imdbUrl).get_url()
+        match = re.search('/title/(.+?)/', url)
+        if match:
+                imdbId = match.group(1)
+        return imdbId        
+
+        
 def GetLinks(url, title='', year='0'): # Get Links
         print 'In GetLinks %s' % url
         html = net.http_GET(url).content
@@ -174,8 +295,6 @@ def GetDomain(url):
 
 
 def GetMediaInfo(title, year):
-        print title
-        print year
         listitem = xbmcgui.ListItem()
         listitem.setInfo('video', {'Title': title, 'Year': int(year) } )
         return listitem
@@ -187,11 +306,15 @@ def Categories():  #categories
         for url, title in match:
                 if 'Adult' in title and hideAdult:
                         continue
-                addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': '1', 'numOfPages': '2'}, {'title':  title})
+
+                if 'tv-packs' in url or 'Adult' in title:
+                        addon.add_directory({'mode': 'GetTitles', 'section': 'tvshow', 'url': url, 'startPage': '1', 'numOfPages': '1'}, {'title':  title})
+                else:
+                        addon.add_directory({'mode': 'GetTitles', 'section': 'movie', 'url': url, 'startPage': '1', 'numOfPages': '1'}, {'title':  title})
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def MainMenu():    #homescreen
-        addon.add_directory({'mode': 'GetTitles', 'url': BASE_URL, 'startPage': '1', 'numOfPages': '2'}, {'title':  'Homepage'})
+        addon.add_directory({'mode': 'GetTitles', 'section': 'movie', 'url': BASE_URL, 'startPage': '1', 'numOfPages': '1'}, {'title':  'Homepage'})
         addon.add_directory({'mode': 'Categories'},  {'title':  'Categories'})
         addon.add_directory({'mode': 'GetSearchQuery'},  {'title':  'Search'})
         addon.add_directory({'mode': 'ResolverSettings'}, {'title':  'Resolver Settings'})
@@ -212,22 +335,15 @@ def GetSearchQuery():
 	else:
                 return
 
-        
 def Search(query):
-        url = 'http://www.google.com/search?q=site:oneclickmoviez.com ' + query
-        url = url.replace(' ', '+')
-        html = net.http_GET(url).content
-        match = re.compile('<h3 class="r"><a href="(.+?)".+?onmousedown=".+?">(.+?)</a>').findall(html)
-        for url, title in match:
-                title = title.replace('<b>...</b>', '').replace('<em>', '').replace('</em>', '')
-                addon.add_directory({'mode': 'GetLinks', 'url': url}, {'title':  title})
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+        url = BASE_URL + '?s='  + query
+        GetTitles('movie', url, startPage= '1', numOfPages= '1')
 
 
 if mode == 'main': 
 	MainMenu()
 elif mode == 'GetTitles': 
-	GetTitles(url, startPage, numOfPages)
+	GetTitles(section, url, startPage, numOfPages)
 elif mode == 'GetLinks':
 	GetLinks(url, title, year)
 elif mode == 'GetSearchQuery':
@@ -240,3 +356,5 @@ elif mode == 'ResolverSettings':
         urlresolver.display_settings()
 elif mode == 'Categories':
         Categories()
+elif mode == 'PlayTrailer':
+        PlayTrailer(url)
